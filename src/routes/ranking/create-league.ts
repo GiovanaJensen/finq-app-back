47175283b1ca51a2952dc230726a882919436436
Giovana Fernandes <oliveirasgiovana143@gmail.com>
2024-12-27 21:19:10 -0300

    add: update user league

--- src/routes/ranking/create-league.ts
+++ src/routes/ranking/create-league.ts
@@ -7,33 +7,49 @@ export async function addUsersToLeague(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().post('/add-users-to-league', {
         schema: {
             body: z.object({
-                level: z.number().min(1),  // Identificador da liga (por exemplo: 1 para Bronze, 2 para Prata, etc.)
-                userIds: z.array(z.string().uuid()),  // Lista de IDs dos usuários a serem atribuídos a essa liga
+                level: z.number().min(1),
+                userIds: z.array(z.string().uuid()),
             })
         }
     }, async (request) => {
         const { level, userIds } = request.body;
 
-        // Verificar se os usuários existem
         const users = await prisma.user.findMany({
             where: {
-                id: { in: userIds }  // Verificar se todos os usuários existem no banco
-            }
+                id: { in: userIds }
+            },
         });
 
         if (users.length !== userIds.length) {
             return { error: "Alguns usuários não foram encontrados" };
         }
 
-        // Criar um nível (Level) para cada usuário
-        const createdLevels = await prisma.level.createMany({
-            data: userIds.map(userId => ({
-                level,  // O mesmo nível (liga)
-                need_points: 0,  // Pode definir os pontos necessários ou deixar como 0, dependendo da sua lógica
-                user_id: userId  // Associando o usuário a essa liga
-            })),
+        const operations = userIds.map(async (userId) => {
+            const existingLevel = await prisma.level.findFirst({
+                where: { user_id: userId }
+            });
+
+            console.log(existingLevel)
+
+            if (existingLevel) {
+                return prisma.level.update({
+                    where: { id: existingLevel.id },
+                    data: { level }
+                });
+            } else {
+                // Cria um novo nível
+                return prisma.level.create({
+                    data: {
+                        level,
+                        need_points: 0,
+                        user_id: userId
+                    }
+                });
+            }
         });
 
-        return { createdLevels };
+        await Promise.all(operations);
+
+        return { message: "Níveis atualizados/criados com sucesso" };
     });
 }
